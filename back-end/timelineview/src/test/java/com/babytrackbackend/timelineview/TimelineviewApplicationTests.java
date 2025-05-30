f608e2b8986401175390e24edeccefebb50cc21f
alexlee-hs <alexlee.hs@gmail.com>
2025-05-29 20:20:33 -0500

    add backend proj

---
+++ back-end/timelineview/src/test/java/com/babytrackbackend/timelineview/TimelineviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babytrackbackend.timelineview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelineviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
